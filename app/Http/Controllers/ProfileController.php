cf4dbf0d02f3fcc562438cc005680e3851f880ee
LekaKoska <159024082+LekaKoska@users.noreply.github.com>
2025-09-09 10:17:54 +0200

    * Adding function changeAvatar * Creating logic for adding images * using Trait

---
+++ app/Http/Controllers/ProfileController.php
@@ -0,0 +1,92 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Requests\NewAvatarRequest;
+use App\Http\Requests\ProfileUpdateRequest;
+use App\Models\User;
+use Illuminate\Http\RedirectResponse;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\File;
+use Illuminate\Support\Facades\Redirect;
+use Illuminate\Support\Facades\Storage;
+use Illuminate\View\View;
+use Intervention\Image\Drivers\Gd\Driver;
+use Intervention\Image\ImageManager;
+use App\Traits\ImageUpload;
+class ProfileController extends Controller
+{
+    use ImageUpload;
+
+    /**
+     * Display the user's profile form.
+     */
+    public function edit(Request $request): View
+    {
+        return view('profile.edit', [
+            'user' => $request->user(),
+        ]);
+    }
+
+    public function changeAvatar(NewAvatarRequest $request)
+    {
+        $avatar = Auth::user()->avatar;
+
+        if($avatar !== null)
+        {
+            File::delete("storage/images/avatars/$avatar");
+        }
+
+
+        //  Trait
+       $name = $this->avatarUpload($request->file('profile_image'), 'images/avatars/');
+
+
+
+            Auth::user()->update(['avatar' => $name]);
+
+            return redirect()->back();
+
+
+
+
+    }
+
+    /**
+     * Update the user's profile information.
+     */
+    public function update(ProfileUpdateRequest $request): RedirectResponse
+    {
+        $request->user()->fill($request->validated());
+
+        if ($request->user()->isDirty('email')) {
+            $request->user()->email_verified_at = null;
+        }
+
+        $request->user()->save();
+
+        return Redirect::route('profile.edit')->with('status', 'profile-updated');
+    }
+
+    /**
+     * Delete the user's account.
+     */
+    public function destroy(Request $request): RedirectResponse
+    {
+        $request->validateWithBag('userDeletion', [
+            'password' => ['required', 'current_password'],
+        ]);
+
+        $user = $request->user();
+
+        Auth::logout();
+
+        $user->delete();
+
+        $request->session()->invalidate();
+        $request->session()->regenerateToken();
+
+        return Redirect::to('/');
+    }
+}
